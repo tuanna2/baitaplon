f29bd642ddf60615cc2e5d389e58a8ed603730cd
tuanna2 <42510591+tuanna2@users.noreply.github.com>
2018-12-16 02:09:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.set("views","./views");
 
 new SocketManager(io);
 
-http.listen(process.env.post || 3000);
+http.listen(process.env.PORT || 3000);
 
 app.get('/',controllerChat.All);
 app.get('/friend',controllerChat.Friend);
